266047828149e3f377fcb80b5179e81244892e88
Denzel S. Williams <49127460+Williamdst@users.noreply.github.com>
2023-07-03 15:24:19 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,6 +25,6 @@ resource "snowflake_database" "demo_db" {
 
 resource "snowflake_schema" "demo_schema" {
   database = snowflake_database.demo_db.name
-  name = "DEMO_SCHEMA"
-  comment = "Schema for Snowflake Terraform demo"
+  name     = "DEMO_SCHEMA"
+  comment  = "Schema for Snowflake Terraform demo"
 }
